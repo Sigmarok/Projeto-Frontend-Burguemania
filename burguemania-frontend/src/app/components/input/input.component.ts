1f74d09683d2be83086228c168b6444a272cf67d
Silas Zames <sigmarok@hotmail.com>
2024-11-18 20:48:49 -0300

    implementação de pedido

--- burguemania-frontend/src/app/components/input/input.component.ts
+++ burguemania-frontend/src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './input.component.css'
 })
 export class InputComponent {
-
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() size: string = 'medium'; // small, medium, large
 }
--- burguemania-frontend/src/app/views/pedido/pedido.component.ts
+++ burguemania-frontend/src/app/views/pedido/pedido.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { InputComponent } from "../../components/input/input.component";
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "../../components/header/header.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-pedido',
   standalone: true,
-  imports: [],
+  imports: [InputComponent, CommonModule, HeaderComponent, NavbarComponent, ButtonComponent],
   templateUrl: './pedido.component.html',
-  styleUrl: './pedido.component.css'
+  styleUrls: ['./pedido.component.css']
 })
 export class PedidoComponent {
+  inputs: { label: string, placeholder: string, size: string }[] = [
+    { label: 'Produto *', placeholder: 'Digite o produto', size: 'medium' },
+    { label: 'Quantidade *', placeholder: 'Digite a quantidade', size: 'medium'}
+  ];
 
+  addInput() {
+    this.inputs.push(
+      { label: 'Produto *', placeholder: 'Digite o produto', size: 'small' },
+      { label: 'Quantidade *', placeholder: 'Digite a quantidade', size: 'small' }
+    );
+  }
+
+  deleteInput() {
+    if (this.inputs.length > 2) {
+      this.inputs.splice(-2, 2); // Remove os últimos dois inputs
+    }
+  }
 }
